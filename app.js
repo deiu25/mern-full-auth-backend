088ed2b6aa843f399ec5caac787a2d3f4ac1b5e3
Deiu <d3iu25@gmail.com>
2024-09-18 16:31:43 +0300

    cors vercel

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://syntaxseeker-auth.vercel.app"], 
+    origin: ["http://localhost:3000", /\.vercel\.app$/],
     credentials: true,
   })
 );
